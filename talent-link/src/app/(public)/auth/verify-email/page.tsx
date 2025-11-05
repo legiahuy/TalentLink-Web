c03ee72fbefa933faa2c4e53f2d01b9093729e91
bonpaul <legiahuy51tambinh@gmail.com>
2025-11-05 23:42:27 +0700

    update otp page

---
+++ talent-link/src/app/(public)/auth/verify-email/page.tsx
@@ -0,0 +1,4 @@
+import { OTPForm } from '@/components/auth/otp-form'
+export default function OTPPage() {
+  return <OTPForm />
+}
--- talent-link/src/services/authService.ts
+++ talent-link/src/services/authService.ts
@@ -28,7 +28,9 @@ export const authService = {
    * Sign up a new user
    */
   signUp: async (params: SignUpParams): Promise<void> => {
-    await axiosClient.post('/auth/signup', params)
+    console.log(params)
+    const res = await axiosClient.post('/auth/signup', params)
+    console.log(res)
   },
 
   /**
